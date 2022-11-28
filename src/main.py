b380ef56bc80dfb3c11a14c3d5bce8d478624a48
JeffTheK <jeff144@protonmail.com>
2022-11-28 23:49:45 +0200

    Add comments

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ import datetime
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
+# структура яка містить статистику гравця після перемоги/програшу
 class Score:
     def __init__(self, total_tiles, bombs_count) -> None:
         self.total_tiles = total_tiles
@@ -19,12 +20,14 @@ class Score:
         self.cleared_tiles = 0
         self.correctly_guessed_bombs = 0
 
+# Годинник який показує скільки пройшло часу з початку гри
 class Timer(Label):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.start_time = datetime.datetime.now()
         Clock.schedule_interval(lambda _: self.update(), 1)
 
+    # Оновлює інтерфейс    
     def update(self):
         self.elapsed_time = datetime.datetime.now() - self.start_time
         s = self.elapsed_time.seconds
@@ -32,10 +35,12 @@ class Timer(Label):
         minutes, seconds = divmod(remainder, 60)
         self.text = '{:02}:{:02}'.format(int(minutes), int(seconds))
 
+    # Перезапускає годинник    
     def start(self):
         self.start_time = datetime.datetime.now()
         self.text = "00:00"
 
+# Клас екрану резулбтату гри
 class ScoreScreen(Screen):
     score: Score = None
 
@@ -43,6 +48,7 @@ class ScoreScreen(Screen):
         self.ids.cleared_tiles_label.text = f"Cleared tiles: {self.score.cleared_tiles}/{self.score.total_tiles - self.score.bombs_count}"
         self.ids.correctly_guessed_bombs_label.text = f"Correctly guessed bombs: {self.score.correctly_guessed_bombs}/{self.score.bombs_count}"
 
+# Комірка яка може містити бомбу
 class Tile(Button):
     def __init__(self, is_bomb, **kwargs):
         super().__init__(**kwargs)
@@ -50,8 +56,12 @@ class Tile(Button):
         self.is_flagged = False
         self.is_revealed = False
 
+# Основний клас де відбувається гра. 
+# Контролює сітку з полями
 class BoardScreen(Screen):
     tiles = []
+
+    # кольори залежно скільки бомб біля комірки
     nearby_bombs_colors = {
         1: (0, 0, 0, 1),
         2: (0, 0, 1, 1),
@@ -63,6 +73,7 @@ class BoardScreen(Screen):
         8: (0, 0, 0, 1),
     }
 
+    # Налаштовує сітку з комірками
     def setup(self, cols, rows, bomb_chance):
         if self.tiles != None:
             for col in self.tiles:
@@ -87,6 +98,8 @@ class BoardScreen(Screen):
                 self.ids.layout.add_widget(tile)
         self.score = Score(cols * rows, bombs_count)
 
+    # Викликається коли гравець натискає на комірку
+    # Саме перевіряє яка з кнопок миші була натиснута
     def on_tile_touch_down(self, col, row, touch):
         tile = self.get_tile_at(col, row)
         if not tile.collide_point(*touch.pos):
@@ -97,6 +110,7 @@ class BoardScreen(Screen):
         elif touch.button == "right":
             self.flag_tile(col ,row)
 
+    # Ставить/видаляє флаг на комірці
     def flag_tile(self, col, row):
         tile = self.get_tile_at(col, row)
 
@@ -117,6 +131,7 @@ class BoardScreen(Screen):
             if tile.is_bomb:
                 self.score.correctly_guessed_bombs += 1
 
+    # Повертає кількість сусідніх комірок з бомбами
     def count_nearby_bombs(self, col, row) -> int:
         print("kek" + str(col) + " " + str(row))
         count = 0
@@ -136,6 +151,8 @@ class BoardScreen(Screen):
 
         return count
     
+    # Викликається якщо натиснута комірка не має бомб.
+    # Рекурсивно викликається на сусідах
     def reveal_non_bomb_tile(self, col, row):
         positions = [
             (col + 1, row),
@@ -160,6 +177,7 @@ class BoardScreen(Screen):
                     self.reveal_non_bomb_tile(pos[0], pos[1])
         self.score.cleared_tiles += 1
     
+    # Викликається коли гравець натиснув і хоче відкрити комірку
     def reveal_tile(self, col, row):
         tile = self.get_tile_at(col, row)
         if tile.is_bomb:
@@ -175,13 +193,16 @@ class BoardScreen(Screen):
         else:
             return self.tiles[col][row]
 
+    # Викликається при поразці
     def on_game_over(self):
         App.get_running_app().root.get_screen("score").score = self.score
         App.get_running_app().root.current = "score"
 
+# Перший екран
 class MainScreen(Screen):
     pass
 
+# Керує всіма так званими екранами (Screen)
 class WindowManager(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
